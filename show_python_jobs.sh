cee41055e54b98e3b83e0e8789d6ce415321373a
Dr. Neil R Voss <vosslab@users.noreply.github.com>
2026-08-21 16:12:50 -0500

    cleaner look

--- show_python_jobs.sh
+++ show_python_jobs.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-ps -ef | grep Python | sed 's#/opt/homebrew/Cellar/python@3.12/3.12.13_2/Frameworks/Python.framework/Versions/3.12/Resources/Python.app/Contents/MacOS/##' | grep -v grep | grep -v sed
+ps -axo pid,ppid,time,command \
+  | grep Python \
+  | sed 's#/opt/homebrew/Cellar/python@3.12/3.[0-9._]*/Frameworks/Python.framework/Versions/3.12/Resources/Python.app/Contents/MacOS/##' \
+  | grep -v grep \
+  | grep -v sed
 echo ""
 uptime
